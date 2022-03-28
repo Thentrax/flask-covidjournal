2324fcf1eababcc59e074b034923dc6c2fea589b
Thiago Cardoso <85969390+Thentrax@users.noreply.github.com>
2022-03-28 11:59:18 -0300

    style - finishing the page style

--- static/js/script.js
+++ static/js/script.js
@@ -34,3 +34,26 @@ function mostrarTodas() {
     });
 
 }
+
+function pesquisarNoticia() {
+    var input = document.getElementById("search").value;
+    input= input.toLowerCase();
+    var cards = document.getElementsByClassName('card');
+    var title = document.getElementsByClassName('card-title');
+    var text = document.getElementsByClassName('card-text');
+
+    var msg = document.querySelector('#msg')
+    msg.innerHTML = "";
+    
+    for (i = 0; i < cards.length; i++) {
+
+        if (!title[i].innerHTML.toLowerCase().includes(input) && (!text[i].innerHTML.toLowerCase().includes(input))) {
+            cards[i].style.display="none";
+        }
+        else{
+            cards[i].style.display="flex";
+            console.log(i, text[i])
+
+        }
+    }
+}
